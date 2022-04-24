7b1ca609a3141207616a9d774e0b5258d58bda0a
SebTota <kidokyo1@gmail.com>
2022-04-24 12:51:42 -0700

    Updated required install to install DAL package

--- setup.py
+++ setup.py
@@ -8,5 +8,5 @@ setuptools.setup(
     description='The Utils package for the Strava Calendar Summary Application',
     url='',
     packages=['strava_calendar_summary_utils'],
-    install_requires=['google-cloud-logging', 'git+https://github.com/SebTota/StravaCalendarSummaryDataAccessLayer.git']
+    install_requires=['google-cloud-logging', 'strava_calendar_summary_data_access @ git+ssh://git@github.com:SebTota/StravaCalendarSummaryUtils.git']
 )
