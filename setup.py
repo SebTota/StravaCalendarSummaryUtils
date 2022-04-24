002a33b3cbfbc9821eb9eb53b1b8e658e4957060
SebTota <kidokyo1@gmail.com>
2022-04-24 12:52:50 -0700

    Updated git package url for reuqired package

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ import setuptools
 
 setuptools.setup(
     name='strava_calendar_summary_utils',
-    version='0.0.3',
+    version='0.0.4',
     author='Sebastian Tota',
     author_email='seb001@protonmail.com',
     description='The Utils package for the Strava Calendar Summary Application',
     url='',
     packages=['strava_calendar_summary_utils'],
-    install_requires=['google-cloud-logging', 'strava_calendar_summary_data_access @ git+ssh://git@github.com:SebTota/StravaCalendarSummaryUtils.git']
+    install_requires=['google-cloud-logging', 'strava_calendar_summary_data_access @ git+https://github.com/SebTota/StravaCalendarSummaryUtils.git']
 )
