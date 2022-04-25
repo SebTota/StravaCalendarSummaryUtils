e770d231a541af15a5b8085425b25a4b70bb2405
SebTota <kidokyo1@gmail.com>
2022-04-24 17:09:22 -0700

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='strava_calendar_summary_utils',
-    version='0.0.6',
+    version='0.0.7',
     author='Sebastian Tota',
     author_email='seb001@protonmail.com',
     description='The Utils package for the Strava Calendar Summary Application',
