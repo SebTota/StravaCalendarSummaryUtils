0f5a24e0fe556f129090272e4380463499727ce9
SebTota <kidokyo1@gmail.com>
2022-04-25 20:59:30 -0700

    Reverted install_requires for DAL

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ import setuptools
 
 setuptools.setup(
     name='strava_calendar_summary_utils',
-    version='0.0.9',
+    version='0.0.10',
     author='Sebastian Tota',
     author_email='seb001@protonmail.com',
     description='The Utils package for the Strava Calendar Summary Application',
     url='',
     packages=setuptools.find_packages(),
-    install_requires=['google-cloud-logging', '-e git+https://github.com/SebTota/StravaCalendarSummaryDataAccessLayer.git#egg=strava_calendar_summary_data_access_layer']
+    install_requires=['google-cloud-logging', 'strava_calendar_summary_data_access_layer@git+https://github.com/SebTota/StravaCalendarSummaryDataAccessLayer.git']
 )
