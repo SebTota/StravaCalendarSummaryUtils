5d02eccf59b30f4671bcfab704c53fde7d6b6239
SebTota <kidokyo1@gmail.com>
2022-04-26 19:55:47 -0700

    Addeed stravalib to requirements

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ import setuptools
 
 setuptools.setup(
     name='strava_calendar_summary_utils',
-    version='0.0.13',
+    version='0.0.14',
     author='Sebastian Tota',
     author_email='seb001@protonmail.com',
     description='The Utils package for the Strava Calendar Summary Application',
     url='',
     packages=setuptools.find_packages(),
-    install_requires=['google-cloud-logging', 'google-cloud-pubsub', 'strava_calendar_summary_data_access_layer'],
+    install_requires=['google-cloud-logging', 'google-cloud-pubsub', 'stravalib', 'strava_calendar_summary_data_access_layer'],
     dependency_links=['https://github.com/SebTota/StravaCalendarSummaryUtils/tarball/master#egg=strava_calendar_summary_data_access_layer']
 )
