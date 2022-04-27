36d0d3a6adc2a4143e91534fba47f3cd903d8df4
SebTota <kidokyo1@gmail.com>
2022-04-26 17:58:37 -0700

    Modified dependecy_link for DAL

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ import setuptools
 
 setuptools.setup(
     name='strava_calendar_summary_utils',
-    version='0.0.12',
+    version='0.0.13',
     author='Sebastian Tota',
     author_email='seb001@protonmail.com',
     description='The Utils package for the Strava Calendar Summary Application',
     url='',
     packages=setuptools.find_packages(),
     install_requires=['google-cloud-logging', 'google-cloud-pubsub', 'strava_calendar_summary_data_access_layer'],
-    dependency_links=['git+https://github.com/SebTota/StravaCalendarSummaryDataAccessLayer.git#egg=strava_calendar_summary_data_access_layer']
+    dependency_links=['https://github.com/SebTota/StravaCalendarSummaryUtils/tarball/master#egg=strava_calendar_summary_data_access_layer']
 )
