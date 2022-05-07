51bff21d07e91b7d9fdc8964a82d8c952d8029f7
SebTota <kidokyo1@gmail.com>
2022-05-07 08:17:47 -0700

    Fixed error being thrown when multiple loggers get created

--- strava_calendar_summary_utils/logging/logging.py
+++ strava_calendar_summary_utils/logging/logging.py
@@ -1,10 +1,11 @@
 from google.cloud.logging import Client as LogClient
 
-class Logging():
+
+class Logging:
     __single = None
-    def __init__( self ):
-        if Logging.__single:
-            raise Logging.__single
-        Logging.__single = self
-        self.logging_client = LogClient()
-        self.logging_client.setup_logging()
+
+    def __init__(self):
+        if not Logging.__single:
+            Logging.__single = self
+            self.logging_client = LogClient()
+            self.logging_client.setup_logging()
