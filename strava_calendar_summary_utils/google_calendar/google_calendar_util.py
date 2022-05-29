e1037e9ec424bf4c5a111f84e0f25b33a7cd7b41
SebTota <kidokyo1@gmail.com>
2022-05-29 10:02:41 -0700

    Added check to see if app calendar already exists before creating a new one

--- strava_calendar_summary_utils/google_calendar/google_calendar_util.py
+++ strava_calendar_summary_utils/google_calendar/google_calendar_util.py
@@ -1,3 +1,5 @@
+from typing import Union
+
 from strava_calendar_summary_data_access_layer import User, UserController
 
 from google.auth.transport.requests import Request
@@ -6,7 +8,7 @@ from google.oauth2.credentials import Credentials
 import datetime
 import logging
 
-SCOPES = ['https://www.googleapis.com/auth/calendar.events']
+SCOPES = ['https://www.googleapis.com/auth/calendar.app.created', 'https://www.googleapis.com/auth/calendar.calendarlist.readonly']
 CALENDAR_NAME = 'Strava Summary'
 
 
@@ -22,14 +24,19 @@ class GoogleCalendarUtil:
         self._user = user
 
         self._refresh_creds_if_needed()
-        self._service = build('calendar', 'v3', credentials=self._calendar_auth)
+        self._service: build = build('calendar', 'v3', credentials=self._calendar_auth)
 
         if self._calendar_id is None:
-            self._calendar_id = self._create_app_calendar(CALENDAR_NAME)
+            self._calendar_id = self._get_app_calendar_id(CALENDAR_NAME)
+            if self._calendar_id is None:
+                self._calendar_id = self._create_app_calendar(CALENDAR_NAME)
             self._save_app_calendar_id(self._calendar_id)
 
-    # Get the calendar id for the app calendar.
-    def get_calendar_id(self):
+    def get_calendar_id(self) -> Union[str, None]:
+        """
+        Retrieve the application's calendar id
+        :return: the calendar id or None if one doesn't yet exist
+        """
         return self._calendar_id
 
     def _before_each_request(self):
@@ -39,7 +46,30 @@ class GoogleCalendarUtil:
         if self._calendar_auth and self._calendar_auth.refresh_token:
             self._calendar_auth.refresh(Request())
 
-    def _create_app_calendar(self, calendar_name):
+    def _get_app_calendar_id(self, calendar_name) -> Union[str, None]:
+        """
+        Retrieve the id of a calendar with the specified name if one exists
+        :param calendar_name: the name of the calendar to look for
+        :return: the id of the calendar if one exists, or None if no match is found
+        """
+        self._before_each_request()
+
+        page_token = None
+        while True:
+            calendars = self._service.calendarList().list(pageToken=page_token).execute()
+            for calendar in calendars['items']:
+                if calendar['summary'] == calendar_name:
+                    print('found existing calendar')
+                    return calendar['id']
+            if not page_token:
+                return None
+
+    def _create_app_calendar(self, calendar_name) -> str:
+        """
+        Create a new calendar
+        :param calendar_name: the name of the calendar
+        :return: id of the calendar created
+        """
         self._before_each_request()
 
         calendar = {
@@ -50,17 +80,38 @@ class GoogleCalendarUtil:
         created_calendar_id = self._service.calendars().insert(body=calendar).execute()['id']
         return created_calendar_id
 
-    def _save_app_calendar_id(self, calendar_id: int):
+    def _save_app_calendar_id(self, calendar_id: str):
+        """
+        Save the calendar id to the signed in user
+        :param calendar_id: the id of the calendar
+        :return: none
+        """
         if self._user is not None and self._user.calendar_id != calendar_id:
             self._user.calendar_id = calendar_id
             UserController().update(self._user.user_id, self._user)
             logging.info('Saved app calendar: {} for user: {}'.format(calendar_id, self._user.user_id))
 
-    def add_all_day_event(self, name: str, description: str, timezone: str, date: str):
+    def add_all_day_event(self, name: str, description: str, timezone: str, date: str) -> Union[str, '-1']:
+        """
+        Add an all day calendar event
+        :param name: name of the calendar event
+        :param description: description of the calendar event
+        :param timezone: timezone of where the event happened
+        :param date: date of the event
+        :return: the id of the calendar event or '-1' on error
+        """
         return self.add_event(name, description, timezone, date, date)
 
-    def add_event(self, name: str, description: str, timezone: str, start: str, end: str):
-
+    def add_event(self, name: str, description: str, timezone: str, start: str, end: str) -> Union[str, '-1']:
+        """
+        Add a new calendar event
+        :param name: name of the calendar event
+        :param description: description of the calendar event
+        :param timezone: timezone of where the event happened
+        :param start: start datetime of the event
+        :param end: end datetime of the event
+        :return: the id of the calendar event or '-1' on error
+        """
         event_body = {
             'summary': name,
             'description': description,
@@ -79,13 +130,22 @@ class GoogleCalendarUtil:
             event_body['start']['dateTime'] = start
             event_body['end']['dateTime'] = end
         else:
-            return -1
+            return '-1'
 
         event = self._service.events().insert(calendarId=self._calendar_id, body=event_body).execute()
         return event.get('id')
 
-    def update_event(self, event_id: str, name: str, description: str, timezone: str, start: str, end: str):
-
+    def update_event(self, event_id: str, name: str, description: str, timezone: str, start: str, end: str) -> Union[str, '-1']:
+        """
+        Update a calendar event for the signed in user
+        :param event_id: the id of the calendar event to update
+        :param name: new name of the calendar event
+        :param description: new description of the calendar event
+        :param timezone: new timezone of where the event happened
+        :param start: new start datetime of the event
+        :param end: new end datetime of the event
+        :return: the id of the calendar event or '-1' on error
+        """
         event_body = {
             'summary': name,
             'description': description,
@@ -104,10 +164,15 @@ class GoogleCalendarUtil:
             event_body['start']['dateTime'] = start
             event_body['end']['dateTime'] = end
         else:
-            return -1
+            return '-1'
 
         event = self._service.events().update(calendarId=self._calendar_id, eventId=event_id, body=event_body).execute()
         return event.get('id')
 
     def delete_event(self, event_id):
+        """
+        Delete a calendar event
+        :param event_id: calendar event id that represents the event that should be deleted
+        :return: none
+        """
         self._service.events().delete(calendarId=self._calendar_id, eventId=event_id).execute()
